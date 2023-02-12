6e76765ba8949288fa9d53d7b528c838435c938d
ahmet bulutluoz <abbulutluoz@gmail.com>
2023-02-12 21:19:51 +0100

    12.gun sonu

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,6 +3,9 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.time.Duration;
 
@@ -10,14 +13,34 @@ public class Driver {
 
     public static WebDriver driver;
 
-    public static WebDriver getDriver(){
+    public static WebDriver getDriver() {
 
-        WebDriverManager.chromedriver().setup();
+        String browser = ConfigReader.getProperty("browser");
+        // configuration.properties dosyasindaki tercih edilen browser'i kaydettik
 
         if (driver == null) {
-            driver = new ChromeDriver();
-        }
 
+            switch (browser) {
+
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "safari":
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+            }
+
+        }
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.manage().window().maximize();
@@ -25,7 +48,17 @@ public class Driver {
         return driver;
     }
 
-    public static void closeDriver(){
-        driver.close();
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.close();
+            driver = null;
+        }
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
     }
 }
